d24e65c3def6bbf0d737839fcd2c1587d9643218
Chirag Mittal <chiragmittal36269@gmail.com>
2023-07-25 12:03:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,221 @@
+const tableHeadRow = document.getElementById("table-head-row");
+
+const tableBody = document.getElementById("table-body");
+
+let currentCell;
+
+const boldButton = document.getElementById("bold-button");
+const italicButton = document.getElementById("italic-button");
+const underlineButton = document.getElementById("underline-button");
+let leftAlignButton = document.getElementById("left-align-button");
+let rightAlignButton = document.getElementById("right-align-button");
+let centerAlignButton = document.getElementById("center-align-button");
+// let justifyAlignButton = document.getElementById("justify-align-button");
+
+const columns = 26;
+const rows = 100;
+
+for (let i = 0; i < columns; i++) {
+	let th = document.createElement("th");
+	th.innerText = String.fromCharCode(i + 65);
+
+	tableHeadRow.appendChild(th);
+}
+
+for (let i = 1; i <= rows; i++) {
+	let tr = document.createElement("tr");
+
+	let th = document.createElement("th");
+	th.innerText = i;
+
+	tr.appendChild(th);
+
+	for (let j = 0; j < columns; j++) {
+		let td = document.createElement("td");
+
+		// td.className = `${i}${String.fromCharCode(j + 65)}`;
+		// td.addEventListener("click", (event) => {
+		// 	console.log(event);
+		// 	console.log(event.target);
+		// 	console.log(event.target.innerHTML);
+		// 	onFocusFn(event);
+		// 	console.log(event.target.className);
+		// 	console.log(td.className);
+		// });
+
+		td.setAttribute("contentEditable", "true");
+		td.setAttribute("id", `${String.fromCharCode(j + 65)}${i}`);
+		td.addEventListener("focus", (event) => {
+			onFocusFn(event);
+		});
+
+		tr.appendChild(td);
+	}
+
+	tableBody.appendChild(tr);
+}
+
+function onFocusFn(event) {
+	// console.log(event);
+	currentCell = event.target;
+	document.getElementById("current-cell").innerText = event.target.id;
+	// currentCell.style.backgroundColor = "lightgrey";
+
+	buttonColors();
+
+	// if (currentCell.style.fontWeight === "bold") {
+	// 	boldButton.style.backgroundColor = "yellow";
+	// } else {
+	// 	boldButton.style.backgroundColor = "transparent";
+	// }
+
+	// if (currentCell.style.fontStyle === "italic") {
+	// 	italicButton.style.backgroundColor = "orange";
+	// } else {
+	// 	italicButton.style.backgroundColor = "transparent";
+	// }
+
+	// if (currentCell.style.textDecoration === "underline") {
+	// 	underlineButton.style.backgroundColor = "green";
+	// } else {
+	// 	underlineButton.style.backgroundColor = "transparent";
+	// }
+}
+
+function buttonColors() {
+	if (currentCell.style.fontWeight === "bold") {
+		boldButton.style.backgroundColor = "grey";
+	} else {
+		boldButton.style.backgroundColor = "transparent";
+	}
+
+	if (currentCell.style.fontStyle === "italic") {
+		italicButton.style.backgroundColor = "grey";
+	} else {
+		italicButton.style.backgroundColor = "transparent";
+	}
+
+	if (currentCell.style.textDecoration === "underline") {
+		underlineButton.style.backgroundColor = "grey";
+	} else {
+		underlineButton.style.backgroundColor = "transparent";
+	}
+
+	if (currentCell.style.textAlign === "left") {
+		leftAlignButton.style.backgroundColor = "grey";
+	} else {
+		leftAlignButton.style.backgroundColor = "transparent";
+	}
+
+	if (currentCell.style.textAlign === "center") {
+		centerAlignButton.style.backgroundColor = "grey";
+	} else {
+		centerAlignButton.style.backgroundColor = "transparent";
+	}
+
+	if (currentCell.style.textAlign === "right") {
+		rightAlignButton.style.backgroundColor = "grey";
+	} else {
+		rightAlignButton.style.backgroundColor = "transparent";
+	}
+
+	// if (currentCell.style.textAlign === "justify") {
+	// 	justifyAlignButton.style.backgroundColor = "grey";
+	// } else {
+	// 	justifyAlignButton.style.backgroundColor = "transparent";
+	// }
+}
+
+boldButton.addEventListener("click", (event) => {
+	// currentCell.style.backgroundColor = "lightblue";
+
+	if (currentCell.style.fontWeight === "bold") {
+		currentCell.style.fontWeight = "normal";
+		boldButton.style.backgroundColor = "transparent";
+	} else {
+		currentCell.style.fontWeight = "bold";
+		boldButton.style.backgroundColor = "grey";
+	}
+
+	// currentCell.style.fontWeight =
+	// 	currentCell.style.fontWeight === "bold" ? "normal" : "bold";
+});
+
+italicButton.addEventListener("click", (event) => {
+	// currentCell.style.backgroundColor = "pink";
+
+	if (currentCell.style.fontStyle === "italic") {
+		currentCell.style.fontStyle = "normal";
+		italicButton.style.backgroundColor = "transparent";
+	} else {
+		currentCell.style.fontStyle = "italic";
+		italicButton.style.backgroundColor = "grey";
+	}
+
+	// currentCell.style.fontStyle =
+	// 	currentCell.style.fontStyle === "italic" ? "normal" : "italic";
+});
+
+underlineButton.addEventListener("click", (event) => {
+	// currentCell.style.backgroundColor = "yellow";
+
+	if (currentCell.style.textDecoration === "underline") {
+		currentCell.style.textDecoration = "none";
+		underlineButton.style.backgroundColor = "transparent";
+	} else {
+		currentCell.style.textDecoration = "underline";
+		underlineButton.style.backgroundColor = "grey";
+	}
+
+	// currentCell.style.textDecoration =
+	// 	currentCell.style.textDecoration === "underline" ? "none" : "underline";
+});
+
+leftAlignButton.addEventListener("click", (event) => {
+	if (currentCell.style.textAlign === "left") {
+		currentCell.style.textAlign = "initial";
+		leftAlignButton.style.backgroundColor = "transparent";
+	} else {
+		currentCell.style.textAlign = "left";
+		leftAlignButton.style.backgroundColor = "grey";
+		centerAlignButton.style.backgroundColor = "transparent";
+		rightAlignButton.style.backgroundColor = "transparent";
+		justifyAlignButton.style.backgroundColor = "transparent";
+	}
+});
+
+centerAlignButton.addEventListener("click", (event) => {
+	if (currentCell.style.textAlign === "center") {
+		currentCell.style.textAlign = "initial";
+		centerAlignButton.style.backgroundColor = "transparent";
+	} else {
+		currentCell.style.textAlign = "center";
+		centerAlignButton.style.backgroundColor = "grey";
+		leftAlignButton.style.backgroundColor = "transparent";
+		rightAlignButton.style.backgroundColor = "transparent";
+		justifyAlignButton.style.backgroundColor = "transparent";
+	}
+});
+
+rightAlignButton.addEventListener("click", (event) => {
+	if (currentCell.style.textAlign === "right") {
+		currentCell.style.textAlign = "initial";
+		rightAlignButton.style.backgroundColor = "transparent";
+	} else {
+		currentCell.style.textAlign = "right";
+		rightAlignButton.style.backgroundColor = "grey";
+		centerAlignButton.style.backgroundColor = "transparent";
+		leftAlignButton.style.backgroundColor = "transparent";
+		justifyAlignButton.style.backgroundColor = "transparent";
+	}
+});
+
+// justifyAlignButton.addEventListener("click", (event) => {
+// 	if (currentCell.style.textAlign === "justify") {
+// 		currentCell.style.textAlign = "initial";
+// 		justifyAlignButton.style.backgroundColor = "transparent";
+// 	} else {
+// 		currentCell.style.textAlign = "justify";
+// 		justifyAlignButton.style.backgroundColor = "grey";
+// 	}
+// });
